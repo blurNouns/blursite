221cbabe1561a8f0a0c0e5738b3776c2b3d92a48
imwylin <iwylie959@gmail.com>
2024-01-09 00:12:38 -0600

    Updates

---
+++ server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"SGKiTq4irXdXpk3apP2YQKxXOrARd3M4csN6SfehQGg=\"}"
---
+++ static/M4a0PHaUakFwQSDYBtap9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-ab9409ef85d63725.js"],"/_error":["static/chunks/pages/_error-e87e5963ec1b8011.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
